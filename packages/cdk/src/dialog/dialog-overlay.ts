9488cb1ea0c2be6b37a482ac2b08736e97611310
JanUnld <j.unnold@gmx.de>
2020-12-19 18:23:08 +0100

    feat(cdk): dialog overlays now need to get subscribed before actually opening sth
    - previously they would be opened during the `DialogOverlay.create` call, leaving any developer without control over the attachment process

--- packages/cdk/src/dialog/dialog-overlay.ts
+++ packages/cdk/src/dialog/dialog-overlay.ts
@@ -2,57 +2,60 @@ import { Overlay, OverlayConfig, OverlayPositionBuilder, ScrollStrategyOptions }
 import { ComponentPortal } from '@angular/cdk/portal';
 import { Injectable, Injector, Type } from '@angular/core';
 import { take } from 'rxjs/operators';
+import { ComponentProps } from '../utils';
 import { ConnectedDialogRef, DialogRef } from './dialog-ref';
 
-/** Describes available options for the creation of dialog overlays */
-export interface DialogConfig<C = any> {
-  disposeOnBackdropClick?: boolean;
+/** Configurable options during the creation of a {@link DialogRef} */
+export interface DialogOverlayConfig<C = any> {
+  injector?: Injector;
   overlay?: OverlayConfig;
-  closable?: boolean;
-  props?: {
-    [P in keyof C]?: C[P] extends Function ? never : C[P];
-  };
+  disposeOnBackdropClick?: boolean;
+  props?: ComponentProps<C>;
 }
 
 @Injectable()
 export class DialogOverlay {
+  /** See {@link Overlay.scrollStrategies} */
   get scrollStrategies(): ScrollStrategyOptions {
     return this.overlay.scrollStrategies;
   }
 
-  constructor(protected overlay: Overlay, protected injector: Injector) {
-  }
+  constructor(protected overlay: Overlay, protected injector: Injector) {}
 
+  /** See {@link Overlay.position} */
   position(): OverlayPositionBuilder {
     return this.overlay.position();
   }
 
-  create<R = any, T = any>(componentType: Type<T>, config?: DialogConfig<T>): ConnectedDialogRef<R, T> {
+  create<R = any, T = any>(componentType: Type<T>, config?: DialogOverlayConfig<T>): ConnectedDialogRef<R, T> {
     const overlayRef = this.overlay.create(this.createOverlayConfig(config || {}));
     const dialogRef = new DialogRef<R>(overlayRef, config);
 
     if (config && config.disposeOnBackdropClick) {
-      overlayRef.backdropClick().pipe(take(1))
+      overlayRef
+        .backdropClick()
+        .pipe(take(1))
         .subscribe(() => dialogRef.dispose());
     }
 
-    const portal = new ComponentPortal(componentType, null, this.createInjector(dialogRef));
+    const portal = new ComponentPortal(componentType, null, this.createInjector(dialogRef, config?.injector));
     return new ConnectedDialogRef(dialogRef, portal);
   }
 
-  createInjector(dialogRef: DialogRef): Injector {
-    return Injector.create({
-      parent: this.injector,
-      providers: [ { provide: DialogRef, useValue: dialogRef } ]
-    });
-  }
-
-  createOverlayConfig(config: DialogConfig = {}): OverlayConfig {
+  createOverlayConfig(config: DialogOverlayConfig = {}): OverlayConfig {
     return new OverlayConfig({
       scrollStrategy: this.scrollStrategies.block(),
       hasBackdrop: true,
 
-      ...config.overlay
+      ...config.overlay,
+    });
+  }
+  createInjector(dialogRef: DialogRef, parent?: Injector): Injector {
+    // we definitely want to force a "default" parent injector even when "null" is given!
+    parent = parent || this.injector;
+    return Injector.create({
+      parent,
+      providers: [{ provide: DialogRef, useValue: dialogRef }],
     });
   }
 }
--- packages/cdk/src/dialog/dialog-ref.ts
+++ packages/cdk/src/dialog/dialog-ref.ts
@@ -1,13 +1,13 @@
 import { OverlayRef } from '@angular/cdk/overlay';
 import { ComponentPortal } from '@angular/cdk/portal';
 import { ComponentRef } from '@angular/core';
-import { Observable, Subject } from 'rxjs';
-import { DialogConfig } from './dialog-overlay';
+import { Observable, Subject, Subscriber, TeardownLogic } from 'rxjs';
+import { DialogOverlayConfig } from './dialog-overlay';
 
 export class DialogRef<R = any> extends Observable<R> {
   protected subject = new Subject<R>();
 
-  constructor(readonly overlayRef: OverlayRef, readonly config?: DialogConfig) {
+  constructor(readonly overlayRef: OverlayRef, readonly config?: DialogOverlayConfig) {
     super((subscriber) => this.subject.subscribe(subscriber));
   }
 
@@ -25,30 +25,38 @@ export class DialogRef<R = any> extends Observable<R> {
 export class ConnectedDialogRef<R = any, T = any> extends DialogRef<R> {
   private _componentRef: ComponentRef<T>;
 
-  get componentRef(): ComponentRef<T> {
+  get componentRef(): ComponentRef<T> | null {
     return this._componentRef;
   }
 
-  constructor(origin: DialogRef, readonly portal: ComponentPortal<T>) {
+  constructor(origin: DialogRef, protected portal: ComponentPortal<T>) {
     super(origin.overlayRef, origin.config);
-    this._componentRef = this.overlayRef.attach(portal);
     this.subject = origin.toSubject();
+  }
+
+  setProps(): void {
     // transfer input property values if there are any
     if (this.config?.props != null) {
-      for (const [ key, value ] of Object.entries(this.config.props)) {
+      for (const [key, value] of Object.entries(this.config.props)) {
         // todo: consider a property validation using the component factory?!
-        this._componentRef.instance[ key ] = value;
+        this._componentRef.instance[key] = value;
       }
     }
   }
 
-  reattach(): void {
-    if (this.overlayRef.hasAttached()) {
+  reattach(): ComponentRef<T> {
+    if (!this.overlayRef.hasAttached()) {
       this._componentRef = this.overlayRef.attach(this.portal);
     }
+    return this._componentRef;
+  }
+  detach(): any {
+    this._componentRef = null;
+    return this.overlayRef.detach();
   }
 
-  detach(): void {
-    this.overlayRef.detach();
+  _subscribe(subscriber: Subscriber<any>): TeardownLogic {
+    this._componentRef = this.overlayRef.attach(this.portal);
+    return super._subscribe(subscriber);
   }
 }
