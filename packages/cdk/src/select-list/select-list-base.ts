ecca5658e823bd1178622f0fe51fb5ae768712c6
JanUnld <j.unnold@gmx.de>
2021-03-10 22:04:35 +0100

    feat(cdk): extended select list base impl (select + toggle all)

--- packages/cdk/src/select-list/select-list-base.ts
+++ packages/cdk/src/select-list/select-list-base.ts
@@ -1,8 +1,16 @@
 import { ContentChildren, Directive, QueryList } from '@angular/core';
 import { SelectionControl } from '../selection/selection-control';
+import { SelectOptions, ToggleOptions } from '../selection/selection-model';
 import { SelectOptionBase } from './select-option-base';
 
 @Directive()
 export abstract class SelectListBase<T> extends SelectionControl<T> {
   @ContentChildren(SelectOptionBase, { descendants: true }) readonly options: QueryList<SelectOptionBase<T>>;
+
+  selectAll(options?: SelectOptions): void {
+    for (const option of this.options.toArray()) option.select(options);
+  }
+  toggleAll(options?: ToggleOptions): void {
+    for (const option of this.options.toArray()) option.toggle(options);
+  }
 }
