7072ce44ae2b6ad3f5c78645c6a9b1a0d4543297
JanUnld <j.unnold@gmx.de>
2021-08-12 22:41:27 +0200

    feat(common): added clip pipe for simplified text and number clipping

---
+++ packages/common/src/pipes/clip/clip-pipe.ts
@@ -0,0 +1,24 @@
+import { NgModule, Pipe, PipeTransform } from '@angular/core';
+
+export function clip<T extends string | number>(value: T, max: number): string {
+  const ELLIPSIS = '\u2026'; // unicode for …
+
+  if (typeof value === 'number') {
+    return `${Math.min(value, max)}+`;
+  } else if (typeof value === 'string') {
+    return `${value.length > max ? value.slice(0, max) : value}${ELLIPSIS}`;
+  } else return value?.toString();
+}
+
+@Pipe({ name: 'clip' })
+export class ClipPipe implements PipeTransform {
+  transform(value: string | number, max: number): string {
+    return clip(value, max);
+  }
+}
+
+@NgModule({
+  declarations: [ClipPipe],
+  exports: [ClipPipe],
+})
+export class ClipPipeModule {}
--- packages/common/src/pipes/index.ts
+++ packages/common/src/pipes/index.ts
@@ -3,3 +3,4 @@ export * from './full-name/full-name-pipe';
 export * from './router/route-data-pipe';
 export * from './router/route-param-pipe';
 export * from './slice/slice-pipe';
+export * from './clip/clip-pipe';
