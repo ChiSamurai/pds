a2c78a3d23c2a576ca2538e2cd69fd60e307263f
ChiSamurai <samurai@gono.info>
2021-08-19 16:23:52 +0200

    commons: clip pipe: fixed missing check if + char should be shown

--- packages/common/src/pipes/clip/clip-pipe.ts
+++ packages/common/src/pipes/clip/clip-pipe.ts
@@ -4,13 +4,13 @@ export function clip<T extends string | number>(value: T, max: number): string {
   const ELLIPSIS = '\u2026'; // unicode for …
 
   if (typeof value === 'number') {
-    return `${Math.min(value, max)}+`;
+    return `${Math.min(value, max)}${value > max ? '+' : ''}`;
   } else if (typeof value === 'string') {
     return `${value.length > max ? value.slice(0, max) : value}${ELLIPSIS}`;
   } else return value?.toString();
 }
 
-@Pipe({ name: 'clip' })
+@Pipe({name: 'clip'})
 export class ClipPipe implements PipeTransform {
   transform(value: string | number, max: number): string {
     return clip(value, max);
@@ -19,6 +19,7 @@ export class ClipPipe implements PipeTransform {
 
 @NgModule({
   declarations: [ClipPipe],
-  exports: [ClipPipe],
+  exports: [ClipPipe]
 })
-export class ClipPipeModule {}
+export class ClipPipeModule {
+}
