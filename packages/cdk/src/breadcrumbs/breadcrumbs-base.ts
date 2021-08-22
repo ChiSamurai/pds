1e40dcb9ec99b1ea9ec19474322f63f6a2465420
JanUnld <j.unnold@gmx.de>
2021-08-23 00:00:41 +0200

    chore(common): improved breadcrumbs base impl
    - added options to skip breadcrumbs site refs when route data is empty

--- packages/cdk/src/breadcrumbs/breadcrumbs-base.ts
+++ packages/cdk/src/breadcrumbs/breadcrumbs-base.ts
@@ -1,17 +1,37 @@
-import { ContentChildren, Directive, QueryList, TemplateRef } from '@angular/core';
+import { coerceBooleanProperty } from '@angular/cdk/coercion';
+import { ContentChildren, Directive, Input, QueryList, TemplateRef } from '@angular/core';
 import { Breadcrumbs, SiteRef } from '@vitagroup/common';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
 import { BreadcrumbDefBase, BreadcrumbDefContext } from './breadcrumb-def-base';
 
 @Directive()
 export class BreadcrumbsBase {
+  private _skipEmptyRouteData = true;
+
   @ContentChildren(BreadcrumbDefBase, { descendants: true })
   protected readonly defs: QueryList<BreadcrumbDefBase>;
+
+  readonly activeSitePath: Observable<SiteRef[]> = this.breadcrumbs.asObservable().pipe(
+    map((sitePath) => {
+      if (this._skipEmptyRouteData) return sitePath.filter((site) => site.route.data != null);
+      else return sitePath;
+    })
+  );
+
+  @Input()
+  set skipEmptyRouteData(value: boolean) {
+    this._skipEmptyRouteData = coerceBooleanProperty(value);
+  }
+  get skipEmptyRouteData(): boolean {
+    return this._skipEmptyRouteData;
+  }
 
   get defaultTemplate(): TemplateRef<BreadcrumbDefContext> | null {
     return this.defs?.find((def) => def.when == null)?.template;
   }
 
-  constructor(readonly breadcrumbs: Breadcrumbs) {}
+  constructor(protected breadcrumbs: Breadcrumbs) {}
 
   resolveTemplate(site: SiteRef): TemplateRef<BreadcrumbDefContext> | null {
     return this.defs?.find((def) => def.when?.(site))?.template || this.defaultTemplate;
