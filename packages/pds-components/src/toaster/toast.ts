5ed775e98811d65a1cfbb0423851071f06356e58
JanUnld <j.unnold@gmx.de>
2021-09-05 14:32:23 +0200

    feat(pds-components): builtin toaster icon di

--- packages/pds-components/src/toaster/toast.ts
+++ packages/pds-components/src/toaster/toast.ts
@@ -1,5 +1,12 @@
-import { Component } from '@angular/core';
-import { ToastRef } from '@vitagroup/cdk';
+import { Component, Inject, InjectionToken, Optional } from '@angular/core';
+import { BuiltInToastType, SvgIconRegistry, ToastRef } from '@vitagroup/cdk';
+
+export interface PdsBuiltInToastIcon {
+  for: BuiltInToastType;
+  iconName: string;
+}
+
+export const PDS_BUILTIN_TOAST_ICON = new InjectionToken<PdsBuiltInToastIcon>('PDS_BUILTIN_TOAST_ICON');
 
 @Component({
   selector: 'pds-toast',
@@ -12,12 +19,29 @@ import { ToastRef } from '@vitagroup/cdk';
     </ng-container>
     <ng-template #builtInToastTemplate>
       <pds-banner class="{{ toastRef.type }}">
-        <svg-icon *ngIf="toastRef.data?.iconName" [name]="toastRef.data.iconName" pdsBefore></svg-icon>
+        <svg-icon *ngIf="iconName" [name]="iconName" pdsBefore></svg-icon>
         <span>{{ toastRef.message }}</span>
       </pds-banner>
     </ng-template>
   `,
 })
 export class PdsToast {
-  constructor(readonly toastRef: ToastRef) {}
+  get iconName(): string {
+    if (this.toastRef.data?.iconName) {
+      return this.toastRef.data.iconName;
+    } else {
+      const iconName = this.findBuiltInToastIcon(this.toastRef.type as BuiltInToastType) || this.toastRef.type;
+      return (this.iconRegistry.has(iconName) && iconName) || null;
+    }
+  }
+
+  constructor(
+    readonly toastRef: ToastRef,
+    protected iconRegistry: SvgIconRegistry,
+    @Optional() @Inject(PDS_BUILTIN_TOAST_ICON) protected builtinIcons?: PdsBuiltInToastIcon[]
+  ) {}
+
+  protected findBuiltInToastIcon(type: BuiltInToastType): string {
+    return this.builtinIcons?.find((def) => def.for === type)?.iconName;
+  }
 }
